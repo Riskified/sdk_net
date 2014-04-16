ac485af02867a0eea7d651474d14d24e3591b5a4
Omri Mosseri <omri@riskified.com>
2014-04-16 15:35:50 +0300

    customer id changed to optional field

--- Riskified.NetSDK/Model/Customer.cs
+++ Riskified.NetSDK/Model/Customer.cs
@@ -11,23 +11,23 @@ namespace Riskified.NetSDK.Model
         /// <summary>
         /// Creates a new Customer
         /// </summary>
-        /// <param name="id">The customer id</param>
         /// <param name="firstName">The customer first name</param>
         /// <param name="lastName">The customer last name</param>
+        /// <param name="id">The customer id (optional)</param>
         /// <param name="ordersCount">The total number of orders made to the merchant by this customer (optional)</param>
         /// <param name="email">The customer email - as registered with (optional) </param>
         /// <param name="verifiedEmail">Signs if the email was verified by the merchant is some way (optional)</param>
         /// <param name="createdAt">The time of creation of the customer card (optional)</param>
         /// <param name="notes">Additional notes regarding the customer (optional)</param>
         /// <exception cref="OrderFieldBadFormatException">Thrown if one or more of the parameters is missing or of bad format</exception>
-        public Customer(int id, string firstName, string lastName, int? ordersCount = null,string email = null, bool? verifiedEmail = null, DateTime? createdAt = null, string notes = null)
+        public Customer(string firstName, string lastName,int? id, int? ordersCount = null,string email = null, bool? verifiedEmail = null, DateTime? createdAt = null, string notes = null)
         {
-            Id = id;
             InputValidators.ValidateValuedString(firstName,"First Name");
             FirstName = firstName;
             InputValidators.ValidateValuedString(lastName, "Last Name");
             LastName = lastName;
             // optional fields
+            Id = id;
             if (!string.IsNullOrEmpty(email))
             {
                 InputValidators.ValidateEmail(email);
@@ -49,7 +49,7 @@ namespace Riskified.NetSDK.Model
         public string FirstName { get; set; }
 
         //todo validate customer ID
-        [JsonProperty(PropertyName = "id", Required = Required.Always)]
+        [JsonProperty(PropertyName = "id", Required = Required.Default, NullValueHandling = NullValueHandling.Ignore)]
         public int? Id { get; set; }
 
         [JsonProperty(PropertyName = "last_name", Required = Required.Always)]
