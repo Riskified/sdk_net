900461756e05044cd3aac38d11a549c21d180a5e
Omri Mosseri <omri@riskified.com>
2015-02-16 14:46:10 +0200

    Cancel reason enum

---
+++ Riskified.SDK/Model/OrderElements/CancelReason.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Riskified.SDK.Model.OrderElements
+{
+    public enum CancelReason
+    {
+        [EnumMember(Value = "customer")]
+        Customer,
+        [EnumMember(Value = "fraud")]
+        Fraud,
+        [EnumMember(Value = "inventory")]
+        Inventory,
+        [EnumMember(Value = "other")]
+        Other
+    }
+}
