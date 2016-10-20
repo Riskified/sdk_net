40a8ddb05d66a42e30de3bb301da4de669a65dc6
riski2 <riski2@IP-C613D6DA>
2016-10-20 11:25:26 +0300

    Added SubmissionReason to order model

---
+++ Riskified.SDK/Model/OrderElements/SubmissionReason.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace Riskified.SDK.Model.OrderElements
+{
+    public enum SubmissionReason
+    {
+        [EnumMember(Value = "failed_verification")]
+        FailedVerification,
+        [EnumMember(Value = "rule_decision")]
+        RuleDecision,
+        [EnumMember(Value = "third_party")]
+        ThirdParty,
+        [EnumMember(Value = "manual_decision")]
+        ManualDecision,
+        [EnumMember(Value = "policy_decision")]
+        PolicyDecision
+    }
+}
