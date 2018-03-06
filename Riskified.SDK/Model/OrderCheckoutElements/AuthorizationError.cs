1aaa78e652b16a0e77d3a52f29ef54f1ebc9dc72
Vinny Martinez <vinny.martinez@riskified.com>
2018-03-06 15:59:43 -0500

    change AuthorizationErrorCode from class to string

--- Riskified.SDK/Model/OrderCheckoutElements/AuthorizationError.cs
+++ Riskified.SDK/Model/OrderCheckoutElements/AuthorizationError.cs
@@ -12,7 +12,7 @@ namespace Riskified.SDK.Model.OrderCheckoutElements
     public class AuthorizationError : IJsonSerializable
     {
 
-        public AuthorizationError(DateTime createdAt, AuthorizationErrorCode errorCode, string message = null)
+        public AuthorizationError(DateTime createdAt, string errorCode, string message = null)
         {
             this.CreatedAt = createdAt;
             this.ErrorCode = errorCode;
@@ -32,8 +32,7 @@ namespace Riskified.SDK.Model.OrderCheckoutElements
         public DateTime? CreatedAt { get; set; }
 
         [JsonProperty(PropertyName = "error_code")]
-        [JsonConverter(typeof(StringEnumConverter))]
-        public AuthorizationErrorCode ErrorCode { get; set; }
+        public string ErrorCode { get; set; }
 
         [JsonProperty(PropertyName = "message")]
         public string Message { get; set; }
