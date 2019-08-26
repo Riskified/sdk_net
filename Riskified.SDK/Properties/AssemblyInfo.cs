fa842df7e38f2f3b989c1c177967e1119ce0ca8d
Moose <mustafa.al-salihi@riskified.com>
2019-08-26 17:15:36 -0400

    Added Auth Result to include liability shift

--- Riskified.SDK/Properties/AssemblyInfo.cs
+++ Riskified.SDK/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Riskified")]
 [assembly: AssemblyProduct("Riskified.SDK")]
-[assembly: AssemblyCopyright("Copyright © Riskified 2012-2018")]
+[assembly: AssemblyCopyright("Copyright © Riskified 2012-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.2.4")]
-[assembly: AssemblyFileVersion("2.1.2.4")]
+[assembly: AssemblyVersion("2.1.2.6")]
+[assembly: AssemblyFileVersion("2.1.2.6")]
