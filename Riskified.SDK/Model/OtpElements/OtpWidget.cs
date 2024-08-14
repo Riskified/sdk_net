8ab51f921b91edfdf518b6469aff9f3657dcbbca
Gering112 <45523369+Gering112@users.noreply.github.com>
2024-08-14 16:45:16 -0400

    ReAdding directory

---
+++ Riskified.SDK/Model/OtpElements/OtpWidget.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Riskified.SDK.Model.OtpElements
+{
+    public class OtpWidgetNotification
+    {
+        [JsonProperty(PropertyName = "widget_token", Required = Required.Always)]
+        public string WidgetToken { get; set; }
+    }
+}
