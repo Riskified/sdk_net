c0e375c8982fb8e6bdcccd4566cdc299cc4741c4
Gering112 <45523369+Gering112@users.noreply.github.com>
2025-12-03 15:55:43 -0500

    updated version

--- Riskified.SDK/Properties/AssemblyInfo.cs
+++ Riskified.SDK/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("4.2.0")]
-[assembly: AssemblyFileVersion("4.2.0")]
+[assembly: AssemblyVersion("4.3.0")]
+[assembly: AssemblyFileVersion("4.3.0")]
 
 
